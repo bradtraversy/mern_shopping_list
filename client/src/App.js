0d51c4e3d55425fd7f47c64c037d3f11dfa75ccf
Brad Traversy <traversymedia@gmail.com>
2018-06-26 12:27:28 -0400

    React client setup

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
