a1622cac3256025a70a132202477c4c1622866cc
Brad Traversy <traversymedia@gmail.com>
2018-06-26 11:48:22 -0400

    Connect to mongo via mongoose

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://brad:brad123@ds117691.mlab.com:17691/mern_shopping'
+};
--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const app = express();
 // Bodyparser Middleware
 app.use(bodyParser.json());
 
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
