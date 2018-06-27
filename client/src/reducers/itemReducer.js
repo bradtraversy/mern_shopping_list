e6a813e1b80af52bc71a332288110b84382015d1
Brad Traversy <traversymedia@gmail.com>
2018-06-26 20:18:44 -0400

    Version 1.0.0

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,4 +1,3 @@
-import uuid from 'uuid';
 import {
   GET_ITEMS,
   ADD_ITEM,
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const items = require('./routes/api/items');
 
@@ -20,6 +21,16 @@ mongoose
 
 // Use Routes
 app.use('/api/items', items);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
