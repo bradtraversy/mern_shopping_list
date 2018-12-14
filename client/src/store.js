d6e3e97b7c52c8898207c5996a80b1c0f41d3c19
Steven Haye <sphred5@gmail.com>
2018-12-14 06:11:50 -0800

    Fixed issue loading on android chrome
    Fixed app not loading on android chrome from https://stackoverflow.com/questions/41333376/reactjs-app-doesnt-work-on-mobile

--- client/src/store.js
+++ client/src/store.js
@@ -1,18 +1,15 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
 const initialState = {};
 
-const middleware = [thunk];
+const middleWare = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, initialState, composeEnhancers(
+  applyMiddleware(...middleWare)
+));
 
 export default store;
